961e1e7feeabc125f4a7ca0543ea06d06b7baa91
yewon-saurus <ywwwon01@gmail.com>
2026-01-10 03:17:18 +0900

    chore: 정책 view에서 가져오기 했지만 사용하지 않는 것 삭제

--- src/components/PrivacyModal.jsx
+++ src/components/PrivacyModal.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
 import {Modal, ModalContent, ModalHeader, ModalBody, useDisclosure} from "@nextui-org/react";
 import PrivacyPolicy from './PrivacyPolicy'
 
 function PrivacyModal() {
-
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   return (
     <>
-        <span onClick={onOpen} style={{fontWeight:'bold', paddingRight: '10px', cursor: 'pointer'}}>개인정보 처리방침</span>
-        <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={true} className='modal'>
+      <span onClick={onOpen} style={{fontWeight:'bold', paddingRight: '10px', cursor: 'pointer'}}>개인정보 처리방침</span>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} isDismissable={true} className='modal'>
         <ModalContent style={{fontFamily: 'JalnanGothic'}} className='modal-content max-h-[90%] max-w-full w-full overflow-y-auto'>
           {(onClose) => (
             <>
--- src/components/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function PrivacyPolicy() {
   return (
     <div>
