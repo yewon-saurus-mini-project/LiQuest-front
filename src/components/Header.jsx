33af428e8adc02a6558205317292720628a06ed4
yewon-saurus <ywwwon01@gmail.com>
2026-01-09 19:43:35 +0900

    chore: Header <a> 태그 -> <Link> 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Sidebar, ConfirmPopup } from "./";
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 import { LuMenu } from "react-icons/lu";
 import { IoMdLogOut } from "react-icons/io";
@@ -40,10 +40,18 @@ function Header({
     }
 
     return (
-        <div style={{fontFamily:'JalnanGothic'}} className={`w-full flex justify-center bg-white z-50 top-0 fixed`}>
-            {logoutPopup.open && <ConfirmPopup onOpenAlert={onClickLogout} onConfirm={handleConfirm} title={logoutPopup.title} message={logoutPopup.message} />}
+        <div style={{ fontFamily:'JalnanGothic' }} className="w-full flex justify-center bg-white z-50 top-0 fixed">
+            {
+                logoutPopup.open &&
+                <ConfirmPopup
+                    onOpenAlert={onClickLogout}
+                    onConfirm={handleConfirm}
+                    title={logoutPopup.title}
+                    message={logoutPopup.message}
+                />
+            }
 
-            <div className="w-[100%] flex justify-between items-center px-4 py-2">
+            <div className="w-full flex justify-between items-center px-4 py-2">
                 <div className="flex items-center gap-[1em]">
                     <div className="lg:hidden">
                         <div className="flex">
@@ -63,20 +71,20 @@ function Header({
                     {
                         isLogin ?
                         <div className="flex items-center gap-[2em]">
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/we"} className="text-[var(--color-primary-900)] hover:text-[var(--color-primary-600)]" >개발팀 소개</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/notice"} className="text-[var(--color-danger-500)] hover:text-[var(--color-primary-600)]" >공지사항</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/board"} className="hover:text-[var(--color-primary-600)]" >커뮤니티</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/rank"} className="flex hover:text-[var(--color-primary-600)]" >전체랭킹</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/myinfo"} className="hover:text-[var(--color-primary-600)]" >{username} 님</a>
+                            <Link to="/we" className="text-[var(--color-primary-900)] hover:text-[var(--color-primary-600)]">개발팀 소개</Link>
+                            <Link to="/notice" className="text-[var(--color-danger-500)] hover:text-[var(--color-primary-600)]">공지사항</Link>
+                            <Link to="/board" className="hover:text-[var(--color-primary-600)]">커뮤니티</Link>
+                            <Link to="/rank" className="flex hover:text-[var(--color-primary-600)]">전체랭킹</Link>
+                            <Link to="/myinfo" className="hover:text-[var(--color-primary-600)]">{username} 님</Link>
                             {/* <a href="" className="hover:text-[var(--color-primary-600)]" >찬스 --개</a> */}
                             {/* <a href="" className="hover:text-[var(--color-primary-600)]" >포인트 --점</a>  */}
                             <button className="flex hover:text-[var(--color-warning-600)]" onClick={onClickLogout}><IoMdLogOut style={{width: '20px', height: 'auto',}} /><span>&nbsp;&nbsp;로그아웃</span></button>
                         </div>
                         :
                         <div className="flex items-center gap-[2em]">
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/we"} className="text-[var(--color-primary-900)] hover:text-[var(--color-primary-600)]" >개발팀 소개</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/notice"} className="text-[var(--color-danger-500)] hover:text-[var(--color-primary-600)]" >공지사항</a>
-                            <a href={import.meta.env.VITE_PUBLIC_URL+"/login"} className="hover:text-[var(--color-primary-600)]">로그인</a>
+                            <Link to="/we" className="text-[var(--color-primary-900)] hover:text-[var(--color-primary-600)]">개발팀 소개</Link>
+                            <Link to="/notice" className="text-[var(--color-danger-500)] hover:text-[var(--color-primary-600)]">공지사항</Link>
+                            <Link to="/login" className="hover:text-[var(--color-primary-600)]">로그인</Link>
                         </div>
                     }
                 </div>
