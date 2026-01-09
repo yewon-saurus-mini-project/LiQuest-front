bca385b3907fd9b4804fce224f34df96105826e6
yewon-saurus <ywwwon01@gmail.com>
2026-01-10 03:53:23 +0900

    chore: QuizItem <a> -> <Link> 수정

--- src/components/QuizItem.jsx
+++ src/components/QuizItem.jsx
@@ -1,4 +1,5 @@
 import { IoChatboxOutline, IoCheckmarkCircleOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const QuizItem = ({ data }) => {
     const {
@@ -8,8 +9,8 @@ const QuizItem = ({ data }) => {
     } = data;
 
     return (
-        <a className="flex gap-2 text-left mx-3 px-4 py-2 hover:bg-[var(--color-primary-100)] rounded-full"
-            href={import.meta.env.VITE_PUBLIC_URL + "/quiz/" + quiz_id}>
+        <Link className="flex gap-2 text-left mx-3 px-4 py-2 hover:bg-[var(--color-primary-100)] rounded-full"
+            to={`/quiz/${quiz_id}`}>
             {
                 solved_date === null ? (
                     <IoChatboxOutline style={{width: '20px', height: 'auto',}} color="var(--color-primary-500)" />
@@ -18,7 +19,7 @@ const QuizItem = ({ data }) => {
                 )
             }
             <span className="text-sm truncate w-full">{quiz_id + "회차: " + word}</span>
-        </a>
+        </Link>
     );
 }
 
