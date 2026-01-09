9166b05d4e2bb34de0221d4f64dc438860e87ac9
yewon-saurus <ywwwon01@gmail.com>
2026-01-09 17:47:13 +0900

    chore: Storybook을 Router로 감싸기 (#3)

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter } from 'react-router-dom';
 import '../src/index.css'
 
 /** @type { import('@storybook/react-vite').Preview } */
@@ -18,5 +19,14 @@ const preview = {
     }
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <BrowserRouter>
+      <Story />
+    </BrowserRouter>
+  ),
+];
+
 
 export default preview;
