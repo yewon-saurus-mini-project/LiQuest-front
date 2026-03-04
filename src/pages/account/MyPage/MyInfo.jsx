b617ab1b3bc5cf9810854eb1863e22977ffbd20f
yewon-saurus <ywwwon01@gmail.com>
2026-03-04 16:59:26 +0900

    view: MyPage/MyInfo story 추가, 컴포넌트 재사용 적용 (#3)

--- src/pages/account/MyPage/MyInfo.jsx
+++ src/pages/account/MyPage/MyInfo.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
-import {Card, CardBody} from "@nextui-org/react";
-import {Input} from "@nextui-org/react";
 import axios from 'axios';
-import Sidebar2 from '../../../components/SideBar2';
+import { Sidebar2, Input } from '../../../components';
 
 function MyInfo() {
     const [userData, setUserData] = useState(null);
@@ -25,55 +23,18 @@ function MyInfo() {
   return (
     <div>
         <Sidebar2 />
-        <div className='pt-[63px] min-h-screen' style={{fontFamily:'JalnanGothic', padding:'63px', marginLeft:'256px', marginBottom:'200px', display: 'flex', justifyContent: 'center', position: 'relative', top: '145px'}}>
-            <Card className="py-4" style={{ width: '520px', height: '430px'}}>
-                <CardBody className="overflow-visible py-2">
-                    <Input
-                        isReadOnly
-                        type="text"
-                        label="이름"
-                        variant="bordered"
-                        value={userData !== null ? `${userData.user.last_name}${userData.user.first_name}` : 'Loading...'}
-                        className="max-w-s"
-                        />
-                    <br />
-                    <Input
-                        isReadOnly
-                        type="text"
-                        label="아이디"
-                        variant="bordered"
-                        value={userData !== null ? `${userData.user.username}` : 'Loading...'}
-                        className="max-w-s"
-                        />
-                    <br />
-                    <Input
-                        isReadOnly
-                        type="email"
-                        label="이메일 주소"
-                        variant="bordered"
-                        value={userData !== null ? `${userData.user.email}` : 'Loading...'}
-                        className="max-w-s"
-                        />
-                    <br />
-                    <Input
-                        isReadOnly
-                        type="text"
-                        label="최근 로그인"
-                        variant="bordered"
-                        value={userData !== null ? new Date(userData.user.last_login).toLocaleString() : 'Loading...'}
-                        className="max-w-s"
-                        />
-                    <br />
-                    <Input
-                        isReadOnly
-                        type="text"
-                        label="가입일"
-                        variant="bordered"
-                        value={userData !== null ? new Date(userData.user.date_joined).toLocaleString() : 'Loading...'}
-                        className="max-w-s"
-                        />
-                </CardBody>
-            </Card>
+        <div className='pt-[63px] min-h-screen' style={{ padding:'63px', marginLeft:'256px', marginBottom:'200px', display: 'flex', justifyContent: 'center', position: 'relative', top: '145px' }}>
+            <div className='w-auto lg:w-1/3'>
+                <Input isReadOnly label='이름' value={userData !== null ? `${userData.user.last_name}${userData.user.first_name}` : 'Loading...'} onChange={() => {}} type='text' />
+                <br />
+                <Input isReadOnly label='아이디' value={userData !== null ? `${userData.user.username}` : 'Loading...'} onChange={() => {}} type='text' />
+                <br />
+                <Input isReadOnly label='이메일 주소' value={userData !== null ? `${userData.user.email}` : 'Loading...'} onChange={() => {}} type='email' />
+                <br />
+                <Input isReadOnly label='최근 로그인' value={userData !== null ? new Date(userData.user.last_login).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
+                <br />
+                <Input isReadOnly label='가입일' value={userData !== null ? new Date(userData.user.date_joined).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
+            </div>
         </div>
     </div>
   )
---
+++ src/pages/account/MyPage/MyInfo.stories.js
@@ -0,0 +1,12 @@
+import MyInfo from './MyInfo';
+
+export default {
+  title: 'Pages/Account/MyPage/MyInfo',
+  component: MyInfo,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {};
