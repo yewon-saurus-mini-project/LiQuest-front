b9976d1cccd0ddd326333dd83c0f564ae916955a
yewon-saurus <ywwwon01@gmail.com>
2026-03-04 18:10:56 +0900

    view: MyInfo Card 디자인(nextui) 다시 살림 (#3)

--- src/pages/account/MyPage/MyInfo.jsx
+++ src/pages/account/MyPage/MyInfo.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Input } from '../../../components';
 import MyPageNav from './MyPageNav';
+import { Card, CardBody } from "@nextui-org/react";
 
 function MyInfo() {
     const [userData, setUserData] = useState(null);
@@ -24,18 +25,20 @@ function MyInfo() {
   return (
     <div>
         <MyPageNav />
-        <div className='pt-[63px] min-h-screen' style={{ padding:'63px', marginLeft:'256px', marginBottom:'200px', display: 'flex', justifyContent: 'center', position: 'relative', top: '145px' }}>
-            <div className='w-auto lg:w-1/3'>
-                <Input isReadOnly label='이름' value={userData !== null ? `${userData.user.last_name}${userData.user.first_name}` : 'Loading...'} onChange={() => {}} type='text' />
-                <br />
-                <Input isReadOnly label='아이디' value={userData !== null ? `${userData.user.username}` : 'Loading...'} onChange={() => {}} type='text' />
-                <br />
-                <Input isReadOnly label='이메일 주소' value={userData !== null ? `${userData.user.email}` : 'Loading...'} onChange={() => {}} type='email' />
-                <br />
-                <Input isReadOnly label='최근 로그인' value={userData !== null ? new Date(userData.user.last_login).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
-                <br />
-                <Input isReadOnly label='가입일' value={userData !== null ? new Date(userData.user.date_joined).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
-            </div>
+        <div className='pt-[63px] min-h-screen' style={{ padding:'63px', marginLeft:'256px', display: 'flex', justifyContent: 'center', position: 'relative' }}>
+            <Card className="py-2 lg:w-1/2 h-fit">
+                <CardBody>
+                    <Input isReadOnly label='이름' value={userData !== null ? `${userData.user.last_name}${userData.user.first_name}` : 'Loading...'} onChange={() => {}} type='text' />
+                    <br />
+                    <Input isReadOnly label='아이디' value={userData !== null ? `${userData.user.username}` : 'Loading...'} onChange={() => {}} type='text' />
+                    <br />
+                    <Input isReadOnly label='이메일 주소' value={userData !== null ? `${userData.user.email}` : 'Loading...'} onChange={() => {}} type='email' />
+                    <br />
+                    <Input isReadOnly label='최근 로그인' value={userData !== null ? new Date(userData.user.last_login).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
+                    <br />
+                    <Input isReadOnly label='가입일' value={userData !== null ? new Date(userData.user.date_joined).toLocaleString() : 'Loading...'} onChange={() => {}} type='text' />
+                </CardBody>
+            </Card>
         </div>
     </div>
   )
