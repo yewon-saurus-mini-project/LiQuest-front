32644a552d5c5fdd6882ef5a7766dc912ad76932
yewon-saurus <ywwwon01@gmail.com>
2026-03-09 02:10:13 +0900

    chore: SignupPage, TermsOfUserForm story 추가 (#3)

---
+++ src/pages/account/SignupPage/SignupPage.stories.js
@@ -0,0 +1,13 @@
+import SignupPage from "./SignupPage";
+
+export default {
+  title: "pages/Account/SignupPage/SignupPage",
+  component: SignupPage,
+  parameters: {
+    layout: "fullscreen",
+  },
+  tags: ["autodocs"],
+  args: {},
+};
+
+export const Default = {};
---
+++ src/pages/account/SignupPage/TermsOfUseForm.stories.js
@@ -0,0 +1,13 @@
+import TermsOfUseForm from "./TermsOfUseForm";
+
+export default {
+  title: "pages/Account/SignupPage/TermsOfUseForm",
+  component: TermsOfUseForm,
+  parameters: {
+    layout: "padded",
+  },
+  tags: ["autodocs"],
+  args: {},
+};
+
+export const Default = {};
