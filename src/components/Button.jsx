8bfce03f738b0450ae814af91552f1a9260b469b
yewon-saurus <ywwwon01@gmail.com>
2026-03-09 02:07:43 +0900

    view: Button 컴포넌트 'customStyle' 적용 방식 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,22 +3,24 @@ const Button = ({
   mode = "primary",
   onClick,
   isLong = false,
-  customStyle = null,
+  customStyle = "",
   ...props
 }) => {
   const styleTable = {
-    "custom": customStyle,
-    "primary": "bg-[var(--color-primary-500)] border-[var(--color-primary-500)] hover:text-[var(--color-primary-500)]",
-    "warning": "bg-[var(--color-warning-500)] border-[var(--color-warning-500)] hover:text-[var(--color-warning-500)]",
-    "danger": "bg-[var(--color-danger-500)] border-[var(--color-danger-500)] hover:text-[var(--color-danger-500)]",
-    "cancle": "bg-gray-300 border-gray-300 !text-black"
+    primary:
+      "bg-[var(--color-primary-500)] border-[var(--color-primary-500)] hover:text-[var(--color-primary-500)]",
+    warning:
+      "bg-[var(--color-warning-500)] border-[var(--color-warning-500)] hover:text-[var(--color-warning-500)]",
+    danger:
+      "bg-[var(--color-danger-500)] border-[var(--color-danger-500)] hover:text-[var(--color-danger-500)]",
+    cancle: "bg-gray-300 border-gray-300 !text-black",
   };
 
   return (
     <button
       type="button"
       onClick={onClick}
-      className={`${styleTable[mode]} ${isLong ? 'w-full' : ''} border transition-colors px-4 py-2 rounded-full text-white text-lg hover:bg-white`}
+      className={`${customStyle} ${styleTable[mode]} ${isLong ? "w-full" : ""} border transition-colors px-4 py-2 rounded-full text-white text-lg hover:bg-white`}
       {...props}
     >
       {children}
